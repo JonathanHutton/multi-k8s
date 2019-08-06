3d0155982a2e979719c7176723530b4daf8b1c3b
JHutton <JHutton@SISFirst.com>
2019-08-06 10:01:45 -0400

    Update worker deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 docker build -t jonathanhutton/multi-client:latest -t jonathanhutton/mulit-client:$SHA -f ./client/Dockerfile ./client
 docker build -t jonathanhutton/multi-server:latest -t jonathanhutton/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jonathanhutton/worker:latest -t jonathanhutton/worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jonathanhutton/worker:latest -t jonathanhutton/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push jonathanhutton/multi-client:latest
 docker push jonathanhutton/multi-client:$SHA
 docker push jonathanhutton/multi-server:latest
 docker push jonathanhutton/multi-server:$SHA
-docker push jonathanhutton/worker:latest
-docker push jonathanhutton/worker:$SHA
+docker push jonathanhutton/multi-worker:latest
+docker push jonathanhutton/multi-worker:$SHA
 
 kubectl apply -f k8s
 
 kubectl set image deployments/server-deployment server=jonathanhutton/multi-server:$SHA
 kubectl set image deployments/client-deployment client=jonathanhutton/multi-client:$SHA
-kubectl set image deployments/worker worker=jonathanhutton/worker:$SHA
+kubectl set image deployments/worker-deployment worker=jonathanhutton/multi-worker:$SHA
